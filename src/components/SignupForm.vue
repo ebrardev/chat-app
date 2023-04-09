47d9acedf5480c6b1da0c3ddaa565b593df22b9e
Ebrar <16155052@mersin.edu.tr>
2023-04-10 01:56:11 +0300

    login and signup component

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="signupform">
+  <form @submit.prevent="handleSubmit">
+    <input type="text" required placeholder="Username" v-model="displayName" />
+    <input type="email" required placeholder="email" v-model="email" />
+    <input type="password" required placeholder="password" v-model="password" />
+    <button class="signup"> Sign up</button>
+  </form>
+</div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+    setup(){
+        const displayName = ref("");
+        const email = ref("");
+        const password = ref("");
+        const handleSubmit = (e) => {
+            console.log(displayName.value, email.value, password.value);
+        }
+        return{
+            displayName,
+            email,
+            password,
+            handleSubmit
+        }
+    
+
+    }
+
+}
+</script>
+
+<style>
+
+
+</style>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,22 +1,56 @@
 <template>
   <div class="welcome container">
-    <p> Welcome</p>
+    <p> Welcome Chattie😍🥵</p>
+    <SignupForm/>
+    <!-- <LoginForm/> -->
   </div>
 </template>
 
 <script>
+import SignupForm from '../components/SignupForm.vue';
+import LoginForm from '../components/LoginForm.vue';
 export default {
+  components: { SignupForm,LoginForm },
+  
 
 }
 </script>
 
 <style>
 
-.welcome {
+.welcome  {
     text-align: center;
     padding: 20px;
-    font-size: 15px;
+ 
 
 }
 
+.welcome form {
+  widows: 300px;
+  margin: 20px auto;
+}
+.welcome label {
+  display: block;
+  margin:20px 0 10px;
+}
+.welcome input {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 20px;
+  outline: none;
+  color: blue;
+  margin: 10px auto;
+}
+
+.welcome button{
+  margin-top: 10px;
+  padding: 10px;
+  background-color: blue;
+  color: white;
+  border: none;
+  border-radius: 6px;
+  outline: none;
+  
+}
 </style>
