bc9428419371c776487a5b6d438665acf438b3ee
Ebrar <16155052@mersin.edu.tr>
2023-04-12 00:32:38 +0300

    route guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/Welcome.vue'
-import Chatroom from '../views/Chatroom.vue'
+import { projectAuth } from '@/firebase/config'
+
+
+const requireAuth=  (to,from,next) =>{
+  let user = projectAuth.currentUser
+  console.log('current user in auth guard: ', user)
+  if(!user){
+    next({name: 'welcome'})
+  }else{
+    next()
+  }
+}
 
 const routes = [
   {
@@ -12,6 +23,7 @@ const routes = [
     path: '/chatroom',
     name: 'chatroom',
     component: () => import('../views/Chatroom.vue')
+    ,beforeEnter: requireAuth
   }
  
 ]
