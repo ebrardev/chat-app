b64f1753bc3db90f1271cc54e7e8edcdfef69c02
Ebrar <16155052@mersin.edu.tr>
2023-04-12 00:43:04 +0300

    authstate and confirmation in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,74 +1,77 @@
 <template>
-  <nav >
-    <div>
-        <p>Welcome {{ user.displayName }} </p>
-        <p  class="email" >Currently logged in as... {{ user.email }}</p>
-    </div>
-    <button @click="handleClick" class="my-navbar-button">Logout</button>
-
-  </nav>
-</template>
-
-<script>
-import useLogout from '../composables/useLogout';
-import getUser from '../composables/getUser';
-export default {
-    setup() {
-        const {logout,error} = useLogout();
-        const {user} = getUser();
-        const handleClick = async () => {
-            await logout();
-            if(!error.value) {
-                window.alert("Çıkış yapıyorsunuz..!");
-                window.location.href = "/";
-            }
-        }
-        return {
-            handleClick,user 
-        }
-    }
-
-}
-</script>
-
-<style >
- nav{
-  padding: 20px;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
- }
- nav p {
-  margin: 2px auto;
-  font-size: 16px;
-  color: #444;
-  text-align: left;
-
- }
- nav p.email{
-  font-size: 14px;
-  color: #888
- }
- 
- .my-navbar-button {
-    background-color:rgb(5, 5, 128);
-    margin: 10px;
-    padding: 10px 15px;
-    text-align: center;
+    <nav>
+      <div>
+          <p>Welcome {{ user.displayName }} </p>
+          <p  class="email" >Currently logged in as... {{ user.email }}</p>
+      </div>
+      <button @click="handleClick" class="my-navbar-button">Logout</button>
+  
+    </nav>
+  </template>
+  
+  <script>
+  import useLogout from '../composables/useLogout';
+  import getUser from '../composables/getUser';
+  export default {
+      setup() {
+          const {logout,error} = useLogout();
+          const {user} = getUser();
+          const handleClick = async () => {
+              const confirmation = confirm("Çıkış yapmak istediğinize emin misiniz?");
+              if (confirmation) {
+                await logout();
+                if (!error.value) {
+                  window.location.href = "/";
+                }
+              }
+          }
+          return {
+              handleClick,user 
+          }
+      }
+  
+  }
+  </script>
+  
+  <style >
+   nav{
+    padding: 20px;
+  
+    display: flex;
+    justify-content: space-between;
     align-items: center;
-    text-transform: uppercase;
-    transition: 0.5s;
-    background-size: 200% auto;
-    color: white;            
-    box-shadow: 0 0 20px #eee;
-    border-radius: 10px;
-    display: inline-block;
-    font-size: 12px;
-}
- 
-.my-navbar-button:hover {
-    background-color: red;
-}
- 
-</style>
+   }
+   nav p {
+    margin: 2px auto;
+    font-size: 16px;
+    color: #444;
+    text-align: left;
+  
+   }
+   nav p.email{
+    font-size: 14px;
+    color: #888
+   }
+   
+   .my-navbar-button {
+      background-color:rgb(5, 5, 128);
+      margin: 10px;
+      padding: 10px 15px;
+      text-align: center;
+      align-items: center;
+      text-transform: uppercase;
+      transition: 0.5s;
+      background-size: 200% auto;
+      color: white;            
+      box-shadow: 0 0 20px #eee;
+      border-radius: 10px;
+      display: inline-block;
+      font-size: 12px;
+  }
+   
+  .my-navbar-button:hover {
+      background-color: red;
+  }
+   
+  </style>
+  
