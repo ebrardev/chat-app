c1dbf18d15a704b06609c9d4a83bbcbe5b80d325
Ebrar <16155052@mersin.edu.tr>
2023-04-12 01:50:15 +0300

    yoruldum kachow

---
+++ src/components/NewChatForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <form>
+    <textarea placeholder="write your message" v-model="message"/>
+  </form>
+</template>
+
+<script>
+import {ref} from 'vue';
+import getUser from "../composable/getUser"
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
